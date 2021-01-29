505f5650a328d29d68411842fac047e04df7b421
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-01-29 14:37:46 +0000

    checkStatus main routine to check the status of the applyRoutine for any errors and display them

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -96,82 +97,6 @@ func terraformApply(folder string) error {
 	return nil
 }
 
-func applyNamespaceDir(done chan string, folder string) {
-	// kubectlApply(folder)
-
-	if filepath.Base(folder) != "resources" {
-
-		kubectlArgs := []string{"-n", filepath.Base(folder), "apply", "-f", folder}
-
-		kubectlCommand := exec.Command("kubectl", kubectlArgs...)
-		kubectlCommand.Stderr = os.Stderr
-		kubectlCommand.Stdout = os.Stdout
-
-		if err := kubectlCommand.Run(); err != nil {
-			fmt.Println(err)
-		}
-
-	}
-
-	// terraformApply(folder)
-
-	if filepath.Base(folder) == "resources" {
-
-		// Get the value of an Environment Variable
-		bucket := os.Getenv("PIPELINE_STATE_BUCKET")
-		key_prefix := os.Getenv("PIPELINE_STATE_KEY_PREFIX")
-		lock_table := os.Getenv("PIPELINE_TERRAFORM_STATE_LOCK_TABLE")
-		region := os.Getenv("PIPELINE_STATE_REGION")
-		cluster := os.Getenv("PIPELINE_CLUSTER")
-
-		//Checking that an environment variable is present or not.
-
-		env_vars := [3]string{
-			"TF_VAR_cluster_name",
-			"TF_VAR_cluster_state_bucket",
-			"TF_VAR_cluster_state_key"}
-
-		for _, env_var := range env_vars {
-
-			// `os.Getenv` cannot differentiate between an explicitly set empty value
-			// and an unset value. `os.LookupEnv` is preferred to `syscall.Getenv`,
-			// but it is only available in go1.5 or newer. We're using Go build tags
-			// here to use os.LookupEnv for >=go1.5
-			_, ok := os.LookupEnv(env_var)
-			if !ok {
-				fmt.Printf("required key %s missing value", env_var)
-			}
-			continue
-		}
-
-		key := key_prefix + cluster + "/" + filepath.Base(filepath.Dir(folder)) + "/terraform.tfstate"
-
-		if err := os.Chdir(folder); err != nil {
-			fmt.Println(err)
-		}
-
-		kubectlArgs := []string{
-			"init",
-			fmt.Sprintf("%s=bucket=%s", "-backend-config", bucket),
-			fmt.Sprintf("%s=key=%s", "-backend-config", key),
-			fmt.Sprintf("%s=dynamodb_table=%s", "-backend-config", lock_table),
-			fmt.Sprintf("%s=region=%s", "-backend-config", region)}
-
-		kubectlCommand := exec.Command("terraform", kubectlArgs...)
-		kubectlCommand.Stderr = os.Stderr
-		kubectlCommand.Stdout = os.Stdout
-
-		if err := kubectlCommand.Run(); err != nil {
-			fmt.Println(err)
-		}
-
-		if err := os.Chdir(root); err != nil {
-			fmt.Println(err)
-		}
-	}
-	done <- "kubectl apply finished"
-}
-
 func listFiles(files *[]string) filepath.WalkFunc {
 	return func(path string, info os.FileInfo, err error) error {
 		if err != nil {
@@ -202,21 +127,114 @@ func listFolders(folders *[]string) filepath.WalkFunc {
 	}
 }
 
+func applyNamespaceDir(folder string) (tferror string, resp string) {
+
+	var outb, errb bytes.Buffer
+
+	if filepath.Base(folder) != "resources" {
+
+		kubectlArgs := []string{"-n", filepath.Base(folder), "apply", "-f", folder}
+
+		kubectlCommand := exec.Command("kubectl", kubectlArgs...)
+
+		kubectlCommand.Stdout = &outb
+		kubectlCommand.Stderr = &errb
+		kubectlCommand.Run()
+
+	}
+
+	if filepath.Base(folder) == "resources" {
+
+		// Get the value of an Environment Variable
+		bucket := os.Getenv("PIPELINE_STATE_BUCKET")
+		key_prefix := os.Getenv("PIPELINE_STATE_KEY_PREFIX")
+		lock_table := os.Getenv("PIPELINE_TERRAFORM_STATE_LOCK_TABLE")
+		region := os.Getenv("PIPELINE_STATE_REGION")
+		cluster := os.Getenv("PIPELINE_CLUSTER")
+
+		//Checking that an environment variable is present or not.
+
+		env_vars := [3]string{
+			"TF_VAR_cluster_name",
+			"TF_VAR_cluster_state_bucket",
+			"TF_VAR_cluster_state_key"}
+
+		for _, env_var := range env_vars {
+
+			// `os.Getenv` cannot differentiate between an explicitly set empty value
+			// and an unset value. `os.LookupEnv` is preferred to `syscall.Getenv`,
+			// but it is only available in go1.5 or newer. We're using Go build tags
+			// here to use os.LookupEnv for >=go1.5
+			_, ok := os.LookupEnv(env_var)
+			if !ok {
+				return
+			}
+
+			continue
+		}
+
+		key := key_prefix + cluster + "/" + filepath.Base(filepath.Dir(folder)) + "/terraform.tfstate"
+
+		os.Chdir(folder)
+
+		kubectlArgs := []string{
+			"init",
+			fmt.Sprintf("%s=bucket=%s", "-backend-config", bucket),
+			fmt.Sprintf("%s=key=%s", "-backend-config", key),
+			fmt.Sprintf("%s=dynamodb_table=%s", "-backend-config", lock_table),
+			fmt.Sprintf("%s=region=%s", "-backend-config", region)}
+
+		Command := exec.Command("terraform", kubectlArgs...)
+
+		Command.Stdout = &outb
+		Command.Stderr = &errb
+		Command.Run()
+
+		os.Chdir(root)
+	}
+
+	return errb.String(), outb.String()
+}
+
 func main() {
 
-	var folders []string
+	type Result struct {
+		Error    string
+		Response string
+	}
+	checkStatus := func(done <-chan interface{}, folders ...string) <-chan Result {
+		results := make(chan Result)
+		go func() {
+			defer close(results)
 
-	done := make(chan string) // make the "done" channel
+			for _, folder := range folders {
+				var result Result
+				err, resp := applyNamespaceDir(folder)
+				result = Result{Error: err, Response: resp}
+				select {
+				case <-done:
+					return
+				case results <- result:
+				}
+			}
+		}()
+		return results
+	}
+	done := make(chan interface{})
+	defer close(done)
+
+	var folders []string
 
 	err := filepath.Walk(root, listFolders(&folders))
 	if err != nil {
 		panic(err)
 	}
-
-	for _, folder := range folders {
-		go applyNamespaceDir(done, folder)
+	for result := range checkStatus(done, folders...) {
+		if result.Error != "" {
+			fmt.Printf("error: %v", result.Error)
+			continue
+		}
+		fmt.Printf("Response: %v\n", result.Response)
 	}
 
-	msg := <-done // receive from the channel
-	fmt.Println(msg)
 }
